350f174ed805a6d087e958cd5d4927553236455f
Kolotev, Andrei <kolotev@ncbi.nlm.nih.gov>
2022-06-16 17:55:40 -0400

    loader.py: decoding of .b64 fields was added.

--- src/cloudpmc_proto_firestore_loader/loader.py
+++ src/cloudpmc_proto_firestore_loader/loader.py
@@ -1,9 +1,12 @@
+import copy
 import json
+import pprint
 import timeit
 
 from cloudpathlib import AnyPath
 from google.cloud import firestore
 
+from .helpers import decode_b64_fields, deep_truncate
 from .logger import logger
 
 # The `project` parameter is optional and represents which project the client
@@ -27,16 +30,34 @@ class FirestoreLoader:
         try:
             with json_file_path.open() as fd:
                 doc = json.load(fd)
-                logger.info(
-                    f"loading content\n{json.dumps(doc, indent=4, sort_keys=True)}"
-                )
-                logger.info(
-                    f"into collection={collection} with docid={docid}"
-                )
-                self._db_.collection(collection).document(docid).set(doc)
+
+                # display truncated base64 decoded  document
+                pp = pprint.PrettyPrinter(indent=4)
+                doc_decoded = decode_b64_fields(doc)
+                doc_for_display = pp.pformat(deep_truncate(copy.deepcopy(doc_decoded)))
+                logger.info(f"loading content\n{doc_for_display}")
+                logger.info(f"into collection={collection} with docid={docid}")
+
+                # load the document into database
+                self._db_.collection(collection).document(docid).set(doc_decoded)
+
         except Exception as e:
             logger.error(str(e))
 
         upload_stopped = timeit.default_timer()
 
         logger.info(f"Completed in {str(upload_stopped - upload_started)} sec.")
+
+
+# TODO: remove following code later.
+# create base64 encoded article header
+# $ cat < 13901.meta.xml | base64 -w0 > 13901.meta.xml.b64
+
+# create standard compressed and base64 encoded article header
+# $ zstd < 13901.meta.xml | base64 -w0 > 13901.meta.xml.zst.b64
+
+# decode
+# $ base64 -d < 13901.meta.xml.b64 > 13901.meta.xml
+
+# decode & decompress
+# $ base64 -d < 13901.meta.xml.zst.b64 | zstd -d > 13901.meta.xml
