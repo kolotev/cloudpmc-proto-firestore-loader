e8a7d06b8ebd080ab7a9ce41e1fc2336593b8f29
Kolotev, Andrei <kolotev@ncbi.nlm.nih.gov>
2022-06-16 17:54:29 -0400

    helpers.py was added.

---
+++ src/cloudpmc_proto_firestore_loader/helpers.py
@@ -0,0 +1,45 @@
+import base64
+from doctest import ELLIPSIS
+from typing import Dict, Any
+
+
+def bytes_to_str(v):
+    return "".join([f"\\x{{{x:02x}}}" if not (32 < x < 128) else chr(x) for x in v])
+
+
+ELLIPSIS_STR = " ..."
+
+
+def deep_truncate(o, max_size=56):
+    """
+    deep_truncate() helper function iterates over all elements
+    of dictionary and truncate values to requested size
+    """
+    if isinstance(o, dict):
+        for k, v in o.items():
+            if isinstance(v, dict):
+                o.update({k: deep_truncate(v)})
+            elif isinstance(v, bytes):
+                new_v = bytes_to_str(v)
+                if len(new_v) > max_size:
+                    new_v = new_v[:max_size] + ELLIPSIS_STR
+                o.update({k: new_v})
+            elif isinstance(v, str) and len(v) > max_size:
+                o.update({k: v[:max_size] + ELLIPSIS_STR})
+
+    return o
+
+
+def decode_b64_fields(o: Dict[str, Any]) -> Dict[str, Any]:
+    """ """
+    if isinstance(o, dict):
+        for k in list(o):
+            if isinstance(k, str) and k.endswith(".b64") and isinstance(o[k], str):
+                v = o.pop(k)
+                new_k = k.rstrip(".b64")
+                new_v = base64.b64decode(v)
+                o.update({new_k: new_v})
+            elif isinstance(o[k], dict):
+                o.update({k: decode_b64_fields(o[k])})
+
+    return o
