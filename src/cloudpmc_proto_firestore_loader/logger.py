dc047a4ddbda00b800c1da6039cf0cab6c9bd0ec
Kolotev, Andrei <kolotev@ncbi.nlm.nih.gov>
2022-06-21 21:02:27 -0400

    logger.py: got regular & debug configs.

--- src/cloudpmc_proto_firestore_loader/logger.py
+++ src/cloudpmc_proto_firestore_loader/logger.py
@@ -2,14 +2,27 @@ import sys
 
 from loguru import logger
 
-config = {
+CONFIG = {
     "handlers": [
         {
             "sink": sys.stderr,
             "format": "{time:YYYY-MM-DDTHH:mm:ss.SSS} - <level>{message}</level>",
             "colorize": True,
+            "level": "INFO",
         },
     ],
 }
 
-logger.configure(**config)
+CONFIG_DEBUG = {
+    "handlers": [
+        {
+            "sink": sys.stderr,
+            "format": (
+                "{time:YYYY-MM-DDTHH:mm:ss.SSS} - <level>{message}</level> "
+                "<{file}:{line}> [elapsed={elapsed}]"
+            ),
+            "colorize": True,
+            "level": "DEBUG",
+        },
+    ]
+}
